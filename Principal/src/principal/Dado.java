2d455e43b7055718ca0806166205fec1d14fc826
labing506 <labing506@labing50612.udistrital.edu.co>
2017-03-01 16:46:02 -0500

    1er aporte

---
+++ Principal/src/principal/Dado.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.Random;
+
+/**
+ *
+ * @author labing506
+ */
+public class Dado {
+        int numero;
+        Random miRam= new Random();
+        
+        public int Lanzar(){
+            do
+            {
+                
+            }while(numero) 
+            
+            return numero;
+        }
+
+    
+}
---
+++ Principal/src/principal/Jugador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author labing506
+ */
+public class Jugador {
+    int billetera;
+
+    public int getBilletera() {
+        return billetera;
+    }
+
+    public void setBilletera(int billetera) {
+        this.billetera = 1000000000;
+    }
+        
+}
+
